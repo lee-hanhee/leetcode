495a3f0130ac7fc299a776d196a3d79d8d199e44
Hanhee Lee <hanhee@hotmail.ca>
2025-04-11 11:54:16 -0400

    linkedlist

--- 00_notes/code/linked_list.py
+++ 00_notes/code/linked_list.py
@@ -20,6 +20,77 @@ class Node:
         self.next = None
 
 class SinglyLinkedList:
-    def __init__(self, data):
-        self.
+    def __init__(self):
+        self.head = None 
+        self.tail = None 
+        self.count = 0 
     
+    def size(self):
+        return self.count
+    
+    def empty(self):
+        return True if self.count == 0 else False
+    
+    def value_at(self, index):
+        # error check 
+        if index < 0 or index >= self.count: 
+            return None
+
+        # Go until at index and return its data
+        current = self.head 
+        for i in range(index): # 0 indexed
+            current = current.next  
+        return current.data 
+    
+    def push_front(self, value):
+        # New node (and move next to current head)
+        new_head = Node(value) 
+        new_head.next = self.head 
+        
+        # New node is new head 
+        self.head = new_head
+   
+        # Special case
+        if self.count == 0: 
+            self.tail = self.head 
+             
+        # Update count
+        self.count += 1
+    
+    
+    def pop_front(self):
+        if self.empty():
+            return None
+        else: 
+            pop_node = self.head 
+            self.head = self.head.next
+            self.count -= 1
+            
+            if self.count == 0: 
+                self.tail = None 
+                
+        return pop_node.data
+    
+    def pop_back(self):
+        return None
+    
+    def front(self):
+        return self.head.data
+    
+    def back(self):
+        return self.tail.data
+    
+    def insert(self, index, value):
+        return None
+        
+    def erase(self, index):
+        return None
+    
+    def value_n_from_end(self, n):
+        return None
+    
+    def reverse(self):
+        return None
+    
+    def remove_value(self, value):
+        return None
